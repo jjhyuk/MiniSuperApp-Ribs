65ed6465980067d0a6091652918c3c710698618c
JinHyeok Jang <jjhyuk15@gmail.com>
2022-12-31 03:04:34 +0900

    Summary

--- MiniSuperApp/AppDelegate/AppDelegate.swift
+++ MiniSuperApp/AppDelegate/AppDelegate.swift
@@ -1,3 +1,15 @@
+/*
+ Riblet: 라우터, 인터랙터, 빌더 의미
+ -> Riblet: 화면 혹은 로직의 흐름의 묵음
+ -> Input: Dependency (ex Topup 리블렛이 필요한 디펜던시의 청사진)
+ -> Output: Listn
+ 
+ Viewless 리블렛 => 비즈니스 로직 위주
+ 화면 전환 등 조건도 리블렛으로 동작
+ 
+ 요런 기능이 컴포지션이라 정리
+ */
+
 import UIKit
 import ModernRIBs
 
@@ -27,3 +39,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 protocol URLHandler: AnyObject {
   func handle(_ url: URL)
 }
+
